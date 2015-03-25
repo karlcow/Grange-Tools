58e317b05a082834b353729f10792a655579e1a9
Karl Dubost <karl+github@la-grange.net>
2015-03-25 19:01:04 +0900

    fixing some ;

--- html2md2mail/h2m.js
+++ html2md2mail/h2m.js
@@ -21,38 +21,38 @@ TODO: Normalize relative links starting with /something or ../foo/bar/
 
 
 // `markdownEscape` escape characters with a special meaning in MarkDown
-function markdownEscape(text){
-    return text.replace(/\s+/g," ").replace(/[\\*_>#]/g,"\\$&");
+function markdownEscape(text) {
+    return text.replace(/\s+/g, " ").replace(/[\\*_>#]/g, "\\$&");
 }
 
 // `repeat`
-function repeat(str,times){
-    return (new Array(times+1)).join(str)
+function repeat(str, times) {
+    return (new Array(times + 1)).join(str);
 }
 
 // `childsToMarkdown` to navigate the children tree
-function childsToMarkdown(tree,mode){
+function childsToMarkdown(tree, mode) {
     var res = "";
-    for(var i=0, l=tree.childNodes.length; i<l; ++i){
+    for (var i = 0, l = tree.childNodes.length; i < l; ++i) {
         res += nodeToMarkdown(tree.childNodes[i], mode);
     }
     return res;
 }
 
 // `nodeToMarkdown` is the core of the conversion
-function nodeToMarkdown(tree,mode){
+function nodeToMarkdown(tree, mode) {
     var nl = "\n\n";
-    if(tree.nodeType == 3){ // Text node
-        return markdownEscape(tree.nodeValue)
-    }else if(tree.nodeType == 1){
-        if(mode == "block"){
+    if (tree.nodeType == 3) { // Text node
+        return markdownEscape(tree.nodeValue);
+    } else if (tree.nodeType == 1) {
+        if (mode == "block") {
             // taking care of block tags
-            switch(tree.tagName.toLowerCase()){
+            switch (tree.tagName.toLowerCase()) {
                 case "br":
                     return nl;
                 case "hr":
                     return nl + "---" + nl;
-                // Block container elements
+                    // Block container elements
                 case "p":
                 case "div":
                 case "section":
@@ -66,41 +66,47 @@ function nodeToMarkdown(tree,mode){
                 case "pre":
                     return nl + "    " + childsToMarkdown(tree, "inline") + nl;
                 case "code":
-                    if(tree.childNodes.length == 1){
+                    if (tree.childNodes.length == 1) {
                         break; // use the inline format
                     }
                     return nl + "    " + childsToMarkdown(tree, "inline") + nl;
-                case "h1": case "h2": case "h3": case "h4": case "h5": case "h6": case "h7":
+                case "h1":
+                case "h2":
+                case "h3":
+                case "h4":
+                case "h5":
+                case "h6":
+                case "h7":
                     return nl + repeat("#", +tree.tagName[1]) + "  " + childsToMarkdown(tree, "inline") + nl;
                 case "blockquote":
                     return nl + "> " + childsToMarkdown(tree, "inline") + nl;
             }
         }
-        if(/^[ou]+$/.test(mode)){
-            if(tree.tagName == "LI"){
-                return "\n" + repeat("  ", mode.length - 1) + (mode[mode.length-1]=="o"?"1. ":"- ") + childsToMarkdown(tree, mode+"l");
-            }else{
+        if (/^[ou]+$/.test(mode)) {
+            if (tree.tagName == "LI") {
+                return "\n" + repeat("  ", mode.length - 1) + (mode[mode.length - 1] == "o" ? "1. " : "- ") + childsToMarkdown(tree, mode + "l");
+            } else {
                 console.log("[toMarkdown] - invalid element at this point " + mode.tagName);
                 return childsToMarkdown(tree, "inline")
             }
-        }else if(/^[ou]+l$/.test(mode)){
-            if(tree.tagName == "UL"){
-                return childsToMarkdown(tree,mode.substr(0,mode.length-1)+"u");
-            }else if(tree.tagName == "OL"){
-                return childsToMarkdown(tree,mode.substr(0,mode.length-1)+"o");
+        } else if (/^[ou]+l$/.test(mode)) {
+            if (tree.tagName == "UL") {
+                return childsToMarkdown(tree, mode.substr(0, mode.length - 1) + "u");
+            } else if (tree.tagName == "OL") {
+                return childsToMarkdown(tree, mode.substr(0, mode.length - 1) + "o");
             }
         }
 
         // Inline tags
-        switch(tree.tagName.toLowerCase()){
+        switch (tree.tagName.toLowerCase()) {
             case "strong":
             case "b":
-                return "**" + childsToMarkdown(tree,"inline") + "**";
+                return "**" + childsToMarkdown(tree, "inline") + "**";
             case "em":
             case "i":
-                return "_" + childsToMarkdown(tree,"inline") + "_";
+                return "_" + childsToMarkdown(tree, "inline") + "_";
             case "code": // Inline version of code
-                return "`" + childsToMarkdown(tree,"inline") + "`";
+                return "`" + childsToMarkdown(tree, "inline") + "`";
             case "a":
                 var link = tree.getAttribute("href");
                 var linkinfo = getLinkIndex(link, linkregistry);
@@ -115,14 +121,14 @@ function nodeToMarkdown(tree,mode){
                 return "";
             default:
                 console.log("[toMarkdown] - undefined element " + tree.tagName)
-                return childsToMarkdown(tree,mode);
+                return childsToMarkdown(tree, mode);
         }
 
     }
 }
 
 // `toMarkdown` output the final converted text into MarkDown
-function toMarkdown(node){
+function toMarkdown(node) {
     return nodeToMarkdown(node, "block").replace(/[\n\s]+/, "\n").replace(/[\n]{2,}/g, "\n");
 }
 
@@ -148,7 +154,7 @@ function createLinksIndex(linkregistry) {
     for (var link in linkregistry) {
         linksummary += '[' + linkregistry[link] + ']: ' + link + '\n';
     }
-    return linksummary
+    return linksummary;
 }
 
 // Selection of Content in Web Page
@@ -190,7 +196,7 @@ function toMail(docbody, doctitle, docdate, docuri) {
     mail += escape('\n\n');
     mail += encodeURIComponent(docbody);
     mail += escape('\n\n');
-    return location.href = mail
+    return location.href = mail;
 }
 
 // Web Document Parameters
