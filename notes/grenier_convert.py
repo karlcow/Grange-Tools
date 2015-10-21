47e0e86a96aac692f51b104fa1744f3374c39500
Karl Dubost <karl+github@la-grange.net>
2015-10-21 15:56:46 +0900

    Fetch a note

--- notes/grenier_convert.py
+++ notes/grenier_convert.py
@@ -10,10 +10,21 @@ MIT License
 
 import sys
 
+import urllib
+
+
+def fetch_note(uri):
+    '''Fetch a note file and return the content.'''
+    file_content = urllib.urlopen(uri)
+    content = file_content.read()
+    return content
+
 
 def main():
     '''core program'''
+    URI = 'file:tests/notes.md'
     # Fetch the content with a URI
+    content = fetch_note(URI)
     # Parse the content
     # Convert in the format of your choice
     # Return the data
