286d94eb061cb4689ce7b6fc6aa70ca2872e5d6e
Karl Dubost <karl+github@la-grange.net>
2015-10-21 14:59:09 +0900

    Program structure

---
+++ notes/grenier_convert.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+grenier_convert.py
+
+Created by Karl Dubost on 2015-10-21.
+Copyright (c) 2015 La Grange. All rights reserved.
+MIT License
+"""
+
+import sys
+
+
+def main():
+    '''core program'''
+    # Fetch the content with a URI
+    # Parse the content
+    # Convert in the format of your choice
+    # Return the data
+
+if __name__ == "__main__":
+    sys.exit(main())
